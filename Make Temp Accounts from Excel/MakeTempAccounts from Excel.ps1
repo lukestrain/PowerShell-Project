459ca17bcf4abb82e956ba69b863b14056742527
lukestrain <luke.strain@outlook.com>
2024-11-19 07:43:07 -0800

    Fixed the Domain of the created users

--- Make Temp Accounts from Excel/MakeTempAccounts from Excel.ps1
+++ Make Temp Accounts from Excel/MakeTempAccounts from Excel.ps1
@@ -1,20 +1,30 @@
 ﻿#Import Excel
 Import-Module ImportExcel
 
-$path = Read-Host "Type the filepath of the excel file " #For Lab its C:\Users\Administrator.ADATUM\Desktop\TempAccounts.xlsx
+$path = Read-Host "Enter the filepath of the excel file " #For Lab its C:\Users\Administrator.ADATUM\Desktop\TempAccounts.xlsx
 #Excel headers have the following info: Organization name, exercise dates and supervisor
 
 $OrgInfo = Import-Excel -path $path -StartRow 2 -EndRow 3
 
 
-#$expDate = Import-Excel -path $path -EndRow 2 -ImportColumns 4 -AsDate 4 #exercise end date from excel + 1 day
 $expDate = $orgInfo.'Expiration Date'
 $OUName = $OrgInfo.OrgName + "_temp"
-$OUPath = Read-Host "Enter the path for the temporary OU" #For lab it's DC=ADATUM,DC=COM
+$Domain = Read-Host "Enter the Domain" #For lab it's DC=ADATUM,DC=COM
+
+#Create domain path from the domain
+$DomainArray = $domain.split(".")
+
+$ct = 0
+while ( $ct -le 1) {
+    $DomainArray[$ct] = $DomainArray[$ct].insert(0, "DC=")
+    $CT += 1    
+}
+$DomainPath = $DomainArray -join ","
 
 
-New-ADOrganizationalUnit -Name $OUName -ProtectedFromAccidentalDeletion $false -path $OUPath #Creates a new OU for the temporary organization
-$NewOUPath = "OU=$OUNAME,$OUPath"
+
+New-ADOrganizationalUnit -Name $OUName -ProtectedFromAccidentalDeletion $false -path $DomainPath #Creates a new OU for the temporary organization
+$NewOU = "OU=$OUNAME,$DomainPath"
 
 $TempPass = ConvertTo-SecureString -AsPlainText "Pa55w.rd" -Force ## Defines the temporary Password variable
 
@@ -29,11 +39,11 @@ forEach($u in $TempAccounts) {
     $first = ($u.'First Name') #first name from excel
     $last = ($u.'Last Name') #last name from excel
     $display = "$first $last"
-    $upn = "$first"+"."+"$last"
+    $sam = "$first" + "." + "$last"
+    $upn = "$sam" + "@$Domain"
 
     $phone = $u.'Phone Number' #phone Number from excel
-    New-ADUser -name $display -GivenName $first -Surname $last -UserPrincipalName $upn -MobilePhone $phone -ChangePasswordAtLogon $true -AccountExpirationDate $expDate -Manager $supervisor -AccountPassword $TempPass -Path "$NewOUPath"
+    New-ADUser -name $display -GivenName $first -Surname $last -DisplayName $display  -UserPrincipalName $upn -SamAccountName $sam -MobilePhone $phone -Enabled $true -AccountPassword $TempPass -ChangePasswordAtLogon $true -AccountExpirationDate $expDate -Manager $supervisor -Path "$NewOU"
 }
-
 
 #Things to do next: Add the Supervisor functionality. I would need to create the supervisor's account from the excel doc, remove it from the list of accounts to be created, store his/her account as the $supervisor variable, then add -manger $supervisor to the new-aduser command in the function
---
+++ PSATTCK/PSAttck Testing.ps1
@@ -0,0 +1,7 @@
+install-module PSATTCK
+Import-Module PSATTCK
+
+
+get-help psattck
+
+get-help about_PSAttck
